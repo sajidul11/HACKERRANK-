f769ea2220d531c5d5415a8745b63da8e5eb90bd
sajidul11 <sajidulislam.official11@gmail.com>
2026-01-14 23:13:04 +0530

    HACKERRANK DAY-14

---
+++ Check if Two Numbers Have Same Last Digit.c
@@ -0,0 +1,14 @@
+int main() {
+int a,b;
+    scanf("%d %d",&a,&b);
+    if(a%10==b%10)
+    {
+        printf("YES");
+     }
+    else{
+        printf("NO");
+            
+        
+    }
+    return 0;
+}
