3daa6b0cd1028daff954603d07de1bf669b1633c
sajidul11 <sajidulislam.official11@gmail.com>
2026-01-15 23:02:27 +0530

    HACKERRANK DAY-15

---
+++ Voting Eligibility.c
@@ -0,0 +1,14 @@
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int age;
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("Eligible");
+    }
+    else{
+        printf("Not Eligible");
+    }
+    return 0;
+}
