5e76403756f0cb6f177ed6faa6941f1f7b511b24
sajidul11 <sajidulislam.official11@gmail.com>
2026-01-25 23:01:20 +0530

    HACKERRANK DAY-23

---
+++ Rounded Value - Remove Decimals.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  
+    float a;
+        scanf("%f",&a);
+    int value = a;
+        printf("Rounded Value = %d",value);
+    return 0;
+}
